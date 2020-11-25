dcba51e72bf4b5769877d37d6686d1e9ed1053e3
Jayson Taylor Thorpe <Jayson Taylor Thorpe@Livingston>
2020-11-24 22:08:17 -0800

    Finally fixed all the deduct() tests

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -18,8 +18,7 @@ public class GiftCardTest {
       balance = 100.00;
       card = new GiftCard(issuingStore, balance);
       
-      assertEquals("getIssuingStore()",
-          issuingStore, card.getIssuingStore());
+      assertEquals("getIssuingStore()",issuingStore, card.getIssuingStore());
   }
   
   @Test
@@ -38,7 +37,7 @@ public class GiftCardTest {
   }
   
   @Test
-  public void deduct(double amount) {
+  public void deductInvalid() {
     
     double balance;
     GiftCard card;
@@ -48,10 +47,77 @@ public class GiftCardTest {
     balance = 100.00;
     card = new GiftCard(issuingStore, balance);
     
+    double amount = -50.00;
+    
+    String result = "Invalid Transaction";
+    
+    assertEquals("deduct()",result, card.deduct(amount));
+    
+  }
+  
+  @Test
+  public void deductOverdraw() {
+    
+    double balance;
+    GiftCard card;
+    int issuingStore;
+    
+    issuingStore = 1337;
+    balance = 100.00;
+    card = new GiftCard(issuingStore, balance);
+    
+    double amount = 150.00;
+    
+    balance -= amount;
+    
+    String result = "Amount Due: " + String.format("%6.2f",
+        Math.abs(balance));
+    
+    assertEquals("deduct()",result,card.deduct(amount));
+    
+  }
+  @Test
+  public void deduct() {
+    
+    double balance;
+    GiftCard card;
+    int issuingStore;
+    
+    issuingStore = 1337;
+    balance = 100.00;
+    card = new GiftCard(issuingStore, balance);
+    
+    double amount = 75.00;
+    
+    balance -= amount;
+    
+    String result = "Remaining Balance: " + String.format("%6.2f",
+        Math.abs(balance));
+    
+    assertEquals("deduct()",result,card.deduct(amount));
+    
+  }
+  
+  @Test
+  public void testBalanceException() {
+    
+    assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+    
+  }
+  
+  @Test
+  public void testStoreIDException() {
     
     
-    assertEquals("deduct()",
-        "Remaining Balance:  50.00", card.deduct(50));
+    assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-1,100.00);});
+    
+  }
+  
+  @Test
+  public void constructor_IncorrectID_High() {
+    
+    assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100.00);});
+
     
   }
 }
